3d06f4cd4e21f9131b2f6582f2bfb7e98242713c
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-30 16:35:25 -0700

    created createBrowserRouter and imported components, updated app.jsx to import nav and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Resume from './components/Resume.jsx';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: 'resume',
+        element: <Resume />
+      },
+    ]
+  }
+
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
