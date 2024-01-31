601442dbb78d5060b38a186f7ce7ad1b0d30b211
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-31 16:23:10 -0700

    updated portfolio page with logo images and links

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,6 +4,9 @@ import jayBoyce from '../assets/boyce.png';
 import weatherApp from '../assets/weatherApp.png';
 import horiseon from '../assets/horiseon.png';
 import nationalPark from '../assets/nationalPark.png';
+import gitHubCat from '../assets/github-mark.png';
+import heroku from '../assets/heroku.svg';
+import netlify from '../assets/netlify-logo.png';
 
 function Portfolio() {
     return (
@@ -12,68 +15,72 @@ function Portfolio() {
                 <h2>My Work</h2>
                 <div className="projects">
                     <div className="project">
-                        <a href="https://get-your-list-together-c8df3b5b0755.herokuapp.com/login" target="_blank" rel="noreferrer">
-                            <div className="project-title">
-                                <h3>Get Your List Together:</h3>
-                                <p>mySQL, Handlebars, Heroku</p>
+                        <div className="project-title">
+                            <h3>Get Your List Together</h3>
+                            <div className='apps'>
+                                <li><a href="https://github.com/britt-young/get_your_list_together" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
+                                <li><a href="https://get-your-list-together-c8df3b5b0755.herokuapp.com/login" target="_blank" rel="noreferrer"><img className="project-apps" src={heroku} alt="heroku logo" /></a></li>
                             </div>
-
-                            <img src={gyltImage} alt="Project Two Image" />
-                        </a>
+                        </div>
+                        <img src={gyltImage} alt="Project Two Image" />
                     </div>
 
                     <div className="project">
-                        <a href="https://jjthedev.github.io/Fitness-App/" target="_blank" rel="noreferrer">
-                            <div className="project-title">
-                                <h3>Healthy Panda:</h3>
-                                <p>JavaScript & API</p>
+                        <div className="project-title">
+                            <h3>Healthy Panda</h3>
+                            <div className='apps'>
+                                <li><a href="https://github.com/JJTheDev/Fitness-App" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
+                                <li><a href="https://jjthedev.github.io/Fitness-App/" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
                             </div>
+                        </div>
 
-                            <img src={healthyPanda} alt="Healthy Panda project image" />
-                        </a>
+                        <img src={healthyPanda} alt="Healthy Panda project image" />
                     </div>
 
                     <div className="project">
-                        <a href="https://jboyce.netlify.app/" target="_blank" rel="noreferrer">
-                            <div className="project-title">
-                                <h3>Jay Boyce:</h3>
-                                <p>JavaScript</p>
+                        <div className="project-title">
+                            <h3>Jay Boyce</h3>
+                            <div className='apps'>
+                                <li><a href="https://github.com/maggiemcc/JBoyce" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
+                                <li><a href="https://jboyce.netlify.app/" target="_blank" rel="noreferrer"><img className="project-apps" src={netlify} alt="netlify logo" /></a></li>
                             </div>
+                        </div>
 
-                            <img src={jayBoyce} alt="Jay Boyce project image"
-                            />
-                        </a>
+                        <img src={jayBoyce} alt="Jay Boyce project image" />
                     </div>
 
                     <div className="project">
-                        <a href="https://maggiemcc.github.io/weather-dashboard/" target="_blank" rel="noreferrer">
-                            <div className="project-title">
-                                <h3>Weather App:</h3>
-                                <p>JavaScript</p>
+                        <div className="project-title">
+                            <h3>Weather App</h3>
+                            <div className='apps'>
+                                <li><a href="https://github.com/maggiemcc/weather-dashboard" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
+                                <li><a href="https://maggiemcc.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
                             </div>
-                            <img src={weatherApp} alt="Weather project image" />
-                        </a>
+                        </div>
+                        <img src={weatherApp} alt="Weather project image" />
                     </div>
 
                     <div className="project">
-                        <a href="https://maggiemcc.github.io/Horiseon/" target="_blank" rel="noreferrer">
-                            <div className="project-title">
-                                <h3>Horiseon:</h3>
-                                <p>HTML/CSS</p>
+                        <div className="project-title">
+                            <h3>Horiseon</h3>
+                            <div className='apps'>
+                                <li><a href="https://github.com/maggiemcc/Horiseon" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
+                                <li><a href="https://maggiemcc.github.io/Horiseon/" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
                             </div>
+                        </div>
 
-                            <img src={horiseon} alt="Horiseon project image" />
-                        </a>
+                        <img src={horiseon} alt="Horiseon project image" />
                     </div>
 
                     <div className="project">
-                        <a href="https://maggie-uvu-projects.netlify.app/national_park/" target="_blank" rel="noreferrer">
-                            <div className="project-title">
-                                <h3>National Park:</h3>
-                                <p>SCSS</p>
+                        <div className="project-title">
+                            <h3>National Park</h3>
+                            <div className='apps'>
+                                <li><a href="https://github.com/maggiemcc/uvu-projects" target="_blank" rel="noreferrer"><img className="project-apps" src={gitHubCat} alt="github logo" /></a></li>
+                                <li><a href="https://maggie-uvu-projects.netlify.app/national_park/" target="_blank" rel="noreferrer"><img className="project-apps" src={netlify} alt="netlify logo" /></a></li>
                             </div>
-                            <img src={nationalPark}alt="National park project image" />
-                        </a>
+                        </div>
+                        <img src={nationalPark} alt="National park project image" />
                     </div>
                 </div>
 
