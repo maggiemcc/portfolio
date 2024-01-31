31867404098a67b68a3a9446d8436223f29fb827
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-31 14:12:17 -0700

    finished contact page validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,48 @@
 import { useState } from 'react';
+
 function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const validateEmail = (email) => {
+        return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+      };
+
+    const handleInput = (event) => {
+        const { target } = event;
+        const formInput = target.name;
+        const inputValue = target.value;
+
+        if (formInput === "name") {
+            setName(inputValue);
+        } else if (formInput === "email") {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        if (!name) {
+            setErrorMessage("Please enter a name");
+            return;
+        }
+        if (!validateEmail(email)) {
+            setErrorMessage("Email is invalid");
+            return
+        }
+        if (!message) {
+            setErrorMessage("Please enter a message");
+            return;
+        }
+        setName("");
+        setEmail("");
+        setMessage("");
+        setErrorMessage("");
+    };
 
     return (
         <main>
@@ -8,32 +51,33 @@ function Contact() {
                 <form>
                     <label>Your Name:</label>
                     <input
-                        value=""
-                        name="userName"
-                        onChange=""
+                        value={name}
+                        name="name"
+                        onChange={handleInput}
                         type="text"
                         id="contact-name"
                         placeholder="John Doe"
                     />
                     <label>Your Email:</label>
                     <input
-                        value=""
-                        name="userEmail"
-                        onChange=""
+                        value={email}
+                        name="email"
+                        onChange={handleInput}
                         type="text"
                         id="contact-email"
                         placeholder="johndoe@email.com"
                     />
                     <label>Your Message:</label>
                     <textarea
-                        value=""
+                        value={message}
                         name="message"
-                        onChange=""
+                        onChange={handleInput}
                         type="message"
                         id="contact-message"
                         placeholder="Your Message"
                     />
-                    <button type='button' onClick="">Submit</button>
+                    <button type='button' onClick={handleFormSubmit}>Submit</button>
+                    {errorMessage && <div className='errorMessage'><p>** {errorMessage} **</p></div>}
                 </form>
             </section>
         </main>
