559fe611c511cde77aa06ecd0a53859ab5d70043
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-31 12:17:27 -0700

    created pages folder, moved pages into pages folder, created nav, updated css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
+
 function Nav() {
+    const currentPage = useLocation().pathname;
+
+    return (
+        <>
+            <header>
+                <h1>Maggie McCausland</h1>
+                <nav>
+                    <ul>
+                        <li><Link to="/" className={currentPage === '/' ? 'nav-link-active' : 'nav-link'}>About Me</Link></li>
+                        <li><Link to="/Portfolio" className={currentPage === '/Portfolio' ? 'nav-link-active' : 'nav-link'}>Portfolio</Link></li>
+                        <li><Link to="/Contact" className={currentPage === '/Contact' ? 'nav-link-active' : 'nav-link'}>Contact</Link></li>
+                        <li><Link to="/Resume" className={currentPage === '/Resume' ? 'nav-link-active' : 'nav-link'}>Resume</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
 }
 export default Nav;
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
-import About from './components/About.jsx';
-import Contact from './components/Contact.jsx';
-import Portfolio from './components/Portfolio.jsx';
-import Resume from './components/Resume.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
 import './index.css'
 
 const router = createBrowserRouter([
