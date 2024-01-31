0704c328695dda7a53437a63c71e4389291751cf
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-31 14:58:52 -0700

    updated resume page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
+import gitHubCat from '../assets/github-mark.png';
+import linkedIn from '../assets/linkedIn.png';
+
 function Footer() {
     return (
         <footer>
+            <div>
+            <li><a href="https://github.com/maggiemcc" target="_blank" rel="noreferrer"><img className="footer-apps" src={gitHubCat} alt="github logo" /></a></li>
+            <li><a href="https://www.linkedin.com/in/maggiemccausland/" target="_blank" rel="noreferrer"><img className="footer-apps" src={linkedIn} alt="linkedIn logo" /></a></li>
+            </div>
             <h6>© Maggie McCausland 2023</h6>
         </footer>
     )
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,3 +1,30 @@
+import resume from '../assets/mccausland-resume.pdf'
 function Resume() {
+    return (
+        <main>
+            <section id="resume">
+                <h2>Resume</h2>
+                <p>Download my <a href={resume} download>resume</a></p>
+
+                <h3>Front-end Proficiencies</h3>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>Response Design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+
+                <h3>Back-end Proficiencies</h3>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </section>
+        </main>
+    );
 }
 export default Resume;
